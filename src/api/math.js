b3df13a3660f580472fa5afb23c95706eddccc01
Jakob Löw <eragonjml@googlemail.com>
2015-07-20 17:24:27 +0200

    added math functions from Util.Math

--- src/api/math.js
+++ src/api/math.js
@@ -43,3 +43,48 @@ cplApi.math.euler = function()
 {
     return 2.72;
 }
+
+//math functions from Util.math
+cplApi.math.pow = function(base, exponent)
+{
+	var next = startNewFunction();
+	var result = new Runtime.Decimal();
+
+	Util.Math.pow(base, exponent, result, next.func);
+
+	next.goNext();
+	return result;
+}
+
+cplApi.math.sin = function(value)
+{
+	var next = startNewFunction();
+	var result = new Runtime.Decimal();
+
+	Util.Math.sin(value, result, next.func);
+
+	next.goNext();
+	return result;
+}
+
+cplApi.math.sqrt = function(value)
+{
+	var next = startNewFunction();
+	var result = new Runtime.Decimal();
+
+	Util.Math.sqrt(value, result, next.func);
+
+	next.goNext();
+	return result;
+}
+
+cplApi.math.factorial = function(value)
+{
+	var next = startNewFunction();
+	var result = new Runtime.Decimal();
+
+	Util.Math.factorial(value, result, next.func);
+
+	next.goNext();
+	return result;
+}
