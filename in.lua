177f94a9b5bf95514d3bb573dbd7edf3e5aa9c7c
Jakob Löw <eragonjml@googlemail.com>
2015-11-11 23:22:30 +0100

    cleaning up old/unused files

--- in.lua
+++
@@ -1,2 +0,0 @@
-radius = 42
-radius = 3 * 7 + radius % 9
--- tests.js
+++
@@ -1,6 +0,0 @@
-var parser = require("luaparse");
-var fs = require("fs");
-
-var src = fs.readFileSync("in.lua").toString();
-var ast = parser.parse(src);
-fs.writeFileSync("out.json", JSON.stringify(ast, undefined, 4));
