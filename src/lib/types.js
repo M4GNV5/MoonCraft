63609fd620b54a6a61a56d148cc5e33125b7139c
Jakob Löw <eragonjml@googlemail.com>
2015-11-13 23:00:00 +0100

    why did i even still have this file?

--- src/lib/types.js
+++ src/lib/types.js
@@ -2,4 +2,3 @@ exports.Integer = require("./../types/Integer.js");
 exports.Boolean = require("./../types/Boolean.js");
 exports.Float = require("./../types/Float.js");
 exports.String = require("./../types/String.js");
-exports.Static = require("./../types/Static.js");
