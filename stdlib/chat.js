f8c9ca4ca42dbc1cce8d4f95024361ff911c9d12
Jakob Löw <eragonjml@googlemail.com>
2015-12-02 20:24:19 +0100

    advanced chat message creation with click events and stuff

--- stdlib/chat.js
+++ stdlib/chat.js
@@ -4,18 +4,64 @@ exports.out = function(val)
     command("tellraw @a [\"Output: \",{0}]".format(val));
 }
 
-exports.create_message = function()
+exports.chat_message = function(text, color, format, click, hover)
+{
+    text = text || "";
+    var msg = {text: text};
+
+    if(color)
+        msg.color = color;
+
+    format = format || {};
+    if(format.bold)
+        msg.bold = format.bold;
+    if(format.italic)
+        msg.italic = format.italic;
+    if(format.underlined)
+        msg.underlined = format.underlined;
+    if(format.strikethrough)
+        msg.strikethrough = format.strikethrough;
+
+    if(click)
+        msg.clickEvent = click;
+    if(hover)
+        msg.hoverEvent = hover;
+
+    return msg;
+}
+
+exports.chat_format = function(bold, italic, underlined, strikethrough)
+{
+    var format = {};
+    if(bold)
+        format.bold = true;
+    if(italic)
+        format.italic = true;
+    if(underlined)
+        format.underlined = true;
+    if(strikethrough)
+        format.strikethrough = true;
+
+    return format;
+}
+
+exports.chat_event = function(action, value)
+{
+    return {action: action, value: value};
+}
+
+exports.chat_message_array = function()
 {
     var extras = [];
     for(var i = 0; i < arguments.length; i++)
-        extras[i] = arguments[i].toTellrawExtra ? arguments[i].toTellrawExtra() : JSON.stringify(arguments[i].toString());
+        extras[i] = arguments[i].toTellrawExtra ? arguments[i].toTellrawExtra() : JSON.stringify(arguments[i]);
 
     return "[{0}]".format(extras.join(","));
 }
 
 exports.tellraw = function()
 {
-    var msg = exports.create_message.apply(exports, arguments);
+    var msg = exports.chat_message_array.apply(exports, arguments);
     command("tellraw @a {0}".format(msg));
 }
 
@@ -23,13 +69,13 @@ exports.tellraw = function()
 
 exports.title = function()
 {
-    var msg = exports.create_message.apply(exports, arguments);
+    var msg = exports.chat_message_array.apply(exports, arguments);
     command("title @a title {0}".format(msg));
 }
 
 exports.subtitle = function()
 {
-    var msg = exports.create_message.apply(exports, arguments);
+    var msg = exports.chat_message_array.apply(exports, arguments);
     command("title @a subtitle {0}".format(msg));
 }
 
