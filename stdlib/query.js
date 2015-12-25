64a93a9fcfcdec58817905be11320ddc905ce638
M4GNV5 <eragonjml@googlemail.com>
2015-12-25 16:24:04 +0100

    use blocks stats rather than the one of the entity

--- stdlib/query.js
+++ stdlib/query.js
@@ -5,8 +5,8 @@ exports.query = function(val, kind, cmd)
     var selector = "@e[type=ArmorStand,tag=query]";
     command("summon ArmorStand ~ ~ ~ {Tags:[\"query\"],NoGravity:true}");
     command("scoreboard players set {0} {1} -1".format(selector, scoreName));
-    command("stats entity {0} set {1} {0} {2}".format(selector, kind, scoreName));
-    command("execute {0} ~ ~ ~ {1}".format(selector, cmd));
+    command("stats block %1:diff% set {1} {0} {2}".format(selector, kind, scoreName));
+    command(cmd);
     command("scoreboard players operation {0} {2} = {1} {2}".format(val.name, selector, scoreName));
     command("kill {0}".format(selector));
 };
